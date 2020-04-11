c9561691825a0e92c464e074c91eacccc5272411
Mike_S_D <9834088@gmail.com>
2020-04-11 20:08:18 +0300

    feedback

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+// 'use strict'
+
+// window.addEventListener('DOMContentLoaded', () => {
+//     const play = document.querySelector('.video__play'),
+//     video = document.querySelector('.wrapper_hidden'),
+//     close = document.querySelector('.close');
+
+//     play.addEventListener('click', () => {
+//         video.classList.remove('hidden');
+//     });
+//     close.addEventListener('click', () => {
+//         video.classList.add('hidden');
+//     });
+
+
+// });
+
+// $(document).ready(function(){
+//     $('.slider__carousel').slick({
+//         infinite: true,
+//         slidesToShow: 6,
+//         slidesToScroll: 1
+//         // prevArrow: '<button type="button" class="slick-prev"><img src="icons/s4_carousel/left.svg"></button>',
+//         // nextArrow: '<button type="button" class="slick-next"><img src="icons/s4_carousel/right.svg"></button>',
+//     });
+
+
+//   });
